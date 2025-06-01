e20f7c1bf6a99312c59025357ab663e02b1294a9
ysk0423 <ssyhrsysk23@gmail.com>
2025-06-01 11:26:54 +0900

    templateの表示確認

--- stream_chat/main.py
+++ stream_chat/main.py
@@ -1,5 +1,5 @@
 import aioredis
-from quart import Quart
+from quart import Quart, render_template
 
 
 app = Quart(__name__)
@@ -13,3 +13,10 @@ STREAM_MAX_LEN = 1000
 @app.before_serving
 async def startup():
     app.redis = await aioredis.from_url(f"redis://{REDIS_HOST}:{REDIS_PORT}")
+
+@app.route('/')
+async def index():
+    return await render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
